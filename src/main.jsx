ad12e341e9b1edc726af4501703f4f3178deeea8
Carlos <casorianog@unadvirtual.edu.co>
2025-07-25 08:29:47 -0500

    reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp'
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
-import { SimpleFormWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
-import { CallBackHook } from './06-memos/CallBackHook'
-import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
+// import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleFormWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { CallBackHook } from './06-memos/CallBackHook'
+// import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
+
+
+import './08-reducer/intro-reducer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -26,6 +29,6 @@ createRoot(document.getElementById('root')).render(
     {/* <Memorize/> */}
     {/* <MemoHook/> */}
     {/* <CallBackHook /> */}
-    <Padre/>
+    {/* <Padre/> */}
   </StrictMode>
 )
